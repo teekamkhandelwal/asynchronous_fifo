a49dd4fe2809de5d5b42e0859db6f0c4be57e8fc
Teekam Chand Khandelwal <teekamkhandelwal@gmail.com>
2021-03-14 22:47:39 +0530

    Create w_ptr_wfull.v

---
+++ w_ptr_wfull.v
@@ -0,0 +1,42 @@
+module wptr_full
+#(
+  parameter ADDRSIZE = 4
+)
+(
+  input   winc, wclk, wrst_n,
+  input   [ADDRSIZE :0] wq2_rptr,
+  output reg  wfull,
+  output  [ADDRSIZE-1:0] waddr,
+  output reg [ADDRSIZE :0] wptr
+);
+
+   reg [ADDRSIZE:0] wbin;
+  wire [ADDRSIZE:0] wgraynext, wbinnext;
+
+  // GRAYSTYLE2 pointer
+  always_ff @(posedge wclk or negedge wrst_n)
+    if (!wrst_n)
+      {wbin, wptr} <= '0;
+    else
+      {wbin, wptr} <= {wbinnext, wgraynext};
+
+  // Memory write-address pointer (okay to use binary to address memory)
+  assign waddr = wbin[ADDRSIZE-1:0];
+  assign wbinnext = wbin + (winc & ~wfull);
+  assign wgraynext = (wbinnext>>1) ^ wbinnext;
+
+  //------------------------------------------------------------------
+  // Simplified version of the three necessary full-tests:
+  // assign wfull_val=((wgnext[ADDRSIZE] !=wq2_rptr[ADDRSIZE] ) &&
+  // (wgnext[ADDRSIZE-1] !=wq2_rptr[ADDRSIZE-1]) &&
+  // (wgnext[ADDRSIZE-2:0]==wq2_rptr[ADDRSIZE-2:0]));
+  //------------------------------------------------------------------
+  assign wfull_val = (wgraynext=={~wq2_rptr[ADDRSIZE:ADDRSIZE-1], wq2_rptr[ADDRSIZE-2:0]});
+
+  always_ff @(posedge wclk or negedge wrst_n)
+    if (!wrst_n)
+      wfull <= 1'b0;
+    else
+      wfull <= wfull_val;
+
+endmodule
